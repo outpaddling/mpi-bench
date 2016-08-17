9d7bec3fbb35530b88da47b58e80b90f17262ac0
outpaddling <bacon4000@gmail.com>
2016-08-17 14:07:56 -0500

    Clean up env scripts

--- env-gcc.sh
+++ env-gcc.sh
@@ -3,6 +3,6 @@
 # Bourne-family shell scripts don't source startup scripts, so we have to
 # do it manually.
 source /etc/bashrc
-module load /sharedapps/modulefiles/pkgsrc/2016Q1
+module purge
 module load /sharedapps/modulefiles/Base-GCC/4.4/openmpi/1.10.2
 
--- env-yum.sh
+++ env-yum.sh
@@ -1,6 +1,7 @@
 
 # Set up environment (PATH, LD_LIBRARY_PATH, etc.) for fluent
 source /etc/bashrc
+module purge
 module use /sharedapps/modulefiles/Base-GCC/4.4/
 module load openmpi/1.8.1-yum
 
